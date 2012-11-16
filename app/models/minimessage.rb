c87330f4c9edb1cdd3f53113eec475ecf11e2973
TBottorff <t.bottorff@cox.net>
2012-11-16 15:44:26 -0600

    Insured that message has user

--- app/models/minimessage.rb
+++ app/models/minimessage.rb
@@ -2,4 +2,5 @@ class Minimessage < ActiveRecord::Base
   attr_accessible :actualMessage, :user_id
   belongs_to :user
   validates :actualMessage, :length => { :maximum => 140}
+  validates :user_id, :presence => true
 end
